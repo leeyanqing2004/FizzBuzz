9e8b90201bb1515bdbe2a30b9a9b75c2a2e39c47
leeyanqing2004 <yanqing.lee@mail.utoronto.ca>
2023-09-15 16:43:26 -0400

    added checkboxes and created main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        FizzBuzzWhile.main();
-
-        FizzBuzzWhile.doFizzBuzz
+        FizzBuzz.main();
+        FizzBuzz.doFizzBuzz(5)
     }
 }
