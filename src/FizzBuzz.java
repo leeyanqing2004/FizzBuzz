18b1126f5a99afe76101f9720b9b0b970f02a61a
leeyanqing2004 <yanqing.lee@mail.utoronto.ca>
2023-09-15 16:40:57 -0400

    added checkboxes and created main.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -33,13 +33,13 @@
     }
 }*/
 
-class FizzBuzzWhile {
+class FizzBuzz {
 
     public static void main(String[] args) {
 
         int i = 1;
 
-        while (i < 100) {
+        while(i < 100) {
 
             i = doFizzBuzz(i);
         }
